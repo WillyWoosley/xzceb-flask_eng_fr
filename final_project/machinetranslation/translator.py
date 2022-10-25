6c7f6bfbc44cde077af8f84f1a49c76116a41f18
WillyWoosley <wdwoosley@gmail.com>
2022-10-25 15:24:27 -0700

    mollified pylint

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,7 +1,8 @@
-import json
+"""Module providing translations services using IBMWatson translation API"""
+import os
+
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -17,7 +18,8 @@ language_translator = LanguageTranslatorV3(
 language_translator.set_service_url(url)
 
 def englishToFrench(englishText):
-    if englishText == None:
+    """Translates English text to French using the IBMWatson cloud service"""
+    if englishText is None:
         raise ValueError("Translation text cannot be None")
 
     if len(englishText) == 0:
@@ -27,21 +29,22 @@ def englishToFrench(englishText):
         text = englishText,
         model_id = "en-fr",
     ).get_result()
-    frenchText = translation["translations"][0]["translation"]
-    
-    return frenchText
+    french_text = translation["translations"][0]["translation"]
+
+    return french_text
 
 def frenchToEnglish(frenchText):
-    if frenchText == None:
+    """Translates French text to English using the IBMWatson cloud service"""
+    if frenchText is None:
         raise ValueError("Translation text cannot be None")
 
     if len(frenchText) == 0:
         return ""
-    
+
     translation = language_translator.translate(
         text = frenchText,
         model_id = "fr-en",
     ).get_result()
-    englishText = translation["translations"][0]["translation"]
+    english_text = translation["translations"][0]["translation"]
 
-    return englishText
+    return english_text
